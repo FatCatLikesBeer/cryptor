1e6e56ef6d957d96528fe2cd3cbca5482b55c2e0
Billy Pamanian <itisbilly@gmail.com>
2025-08-06 22:44:06 -0700

    Separated functions into different files

---
+++ helpers.h
@@ -0,0 +1,11 @@
+/// Prints out help text
+int key_oscillator(char *key_string, int oscindex);
+
+/**
+ * Returns int value of char key at index tracker
+ * Increments/rolls over tracker
+ */
+int str_match(char *str1, char *str2);
+
+/// Returns if 1 if inputs match, else 0
+void print_help(void);
--- main.c
+++ main.c
@@ -1,11 +1,6 @@
+#include "helpers.h"
 #include <stdio.h>
 #include <string.h>
-
-int read_key(char *string);
-int read_flag(char *flag);
-int key_oscillator(char *key_string, int oscindex);
-int str_match(char *str1, char *str2);
-void print_help(void);
 
 int main(int argc, char *argv[]) {
   FILE *fp;
@@ -73,57 +68,4 @@ int main(int argc, char *argv[]) {
 
   fclose(fp);
   return 0;
-}
-
-void print_help(void) {
-  printf("Example:\n");
-  printf(">_ cryptor -e|-d <symetric_key> ./file_to_be_processed.txt # "
-         "Output printeds to stdout\n");
-  printf(">_ cryptor -e encryptionKey ./to_be_encrypted.txt > "
-         "./encrypted_output.txt # Saves output to file\n");
-  printf(">_ cryptor -d encryptionKey ./to_be_decrypted.txt > "
-         "./decrypted_output.txt # Saves output to file\n");
-  printf(">_ cryptor -h # prints this help message\n");
-}
-
-int read_key(char *string) {
-  if (NULL == string) {
-    return 1;
-  }
-  return 0;
-}
-
-int read_flag(char *flag) {
-  if (('e' == flag[1] || 'd' == flag[1]) && '-' == flag[0]) {
-    return 0;
-  };
-  printf("Bad option or no option chosen\n");
-  print_help();
-  return 1;
-}
-
-int key_oscillator(char *key, int tracker) {
-  if ('0' == key[tracker])
-    tracker = 0;
-  return (int)key[tracker++];
-}
-
-// Returns 0 input strings do not match
-int str_match(char *str1, char *str2) {
-  int index = 0;
-  while (str1[index] != '\0') {
-    if (str1[index] != str2[index]) {
-      return 0;
-    }
-    index++;
-  }
-  index = 0;
-  while (str2[index] != '\0') {
-    if (str2[index] != str1[index]) {
-      return 0;
-    }
-    index++;
-  }
-
-  return 1;
 }
